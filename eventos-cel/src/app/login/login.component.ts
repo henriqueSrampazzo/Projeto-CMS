e4e4bb79f855d33370254a24524b472822d87324
henriqueSrampazzo <henriquesr5899@gmail.com>
2019-03-01 16:00:47 -0300

    Testando login com OAuth2

--- eventos-cel/src/app/login/login.component.ts
+++ eventos-cel/src/app/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent implements OnInit {
   private user: Object = {
     'email': '',
     'password': ''
+
   };
 
   constructor(
@@ -24,23 +25,50 @@ export class LoginComponent implements OnInit {
     private router: Router
   ) { }
 
+
+  
+
   ngOnInit() {
+
   }
 
   login() {
     this.http.post('auth/login', this.user)
-            .subscribe(res => {
-              this.storage.set('token', res.token);
-              this.route.queryParams.subscribe(params => {
-                
+      .subscribe(res => {
+        this.storage.set('token', res.token);
+        this.route.queryParams.subscribe(params => {
 
-                if (params.to == 'subscription_confirm') {
-                  return this.router.navigate(['/make-subscription']);
-                } else {
-                  return this.router.navigate(['/painel']);
-                }
-              });
 
-            });
+          if (params.to == 'subscription_confirm') {
+            return this.router.navigate(['/make-subscription']);
+          } else {
+            return this.router.navigate(['/painel']);
+          }
+        });
+
+      });
   }
+
+   onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    console.log("/*/*/*");
+
+  }
+  
+
+
+  // signOut() {
+
+  //   var auth2 = gapi.auth2.getAuthInstance();
+  //   auth2.signOut().then(function () {
+  //     console.log('User signed out.');
+  //   });
+  // }
+
+
 }
